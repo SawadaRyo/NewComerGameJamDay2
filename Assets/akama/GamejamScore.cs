cc007c9da9f304babcd3e237a1c95e5ff5945535
akamasion <siroiro.12.21@gmail.com>
2023-05-26 12:36:11 +0900

    スコア表示機能とスコア加算機能を追加しました

--- Assets/akama/GamejamScore.cs
+++ Assets/akama/GamejamScore.cs
@@ -6,17 +6,21 @@ using UnityEngine.UI;
 
 public class GamejamScore : MonoBehaviour
 {
-    public int score;
+    public int _score;
     public Text ScoreText;
     // Start is called before the first frame update
     void Start()
     {
-        score = 0;//デフォルト値
+        _score = 0;//デフォルト値
     }
 
     // Update is called once per frame
     void Update()
     {
-        ScoreText.text = string.Format("{0}", score);
+        ScoreText.text = string.Format("{0}", _score);
+    }
+    public void Addscore(int score)
+    {
+        _score += score;
     }
 }
