07477bc2e79f26aedf26d195f4be1dbc74e83e81
HIRO_18 <0302yoshihiro@gmail.com>
2023-05-26 14:15:46 +0900

    果物の動き　ゆらゆら修正版

--- Assets/ota/PingPong.cs
+++ Assets/ota/PingPong.cs
@@ -8,12 +8,17 @@ public class PingPong : MonoBehaviour
     [SerializeField] float _speedY = 1.0f;
     [SerializeField] float _amplitudeX = 1.0f;
     [SerializeField] float _amplitudeY = 1.0f;
+    [SerializeField] float _x;
 
+    private void Start()
+    {
+        _x = gameObject.transform.position.x;
+    }
 
 
     void Update()
     {
-        transform.position = new Vector2(   Mathf.Cos(Time.time * _speedX) * _amplitudeX, 
+        transform.position = new Vector2(_x + Mathf.Cos(Time.time * _speedX) * _amplitudeX, 
                                             gameObject.transform.position.y + Mathf.Sin(Time.time * _speedY) * _amplitudeY);
     }
 }
